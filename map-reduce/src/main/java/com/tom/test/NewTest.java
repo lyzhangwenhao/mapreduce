3adabfbe4d5f6561e5cd57a007d0e63817ef5f89
myMiDad <1277420858.c0m>
2019-11-15 21:18:41 +0800

    new

--- map-reduce/src/main/java/com/tom/test/NewTest.java
+++ map-reduce/src/main/java/com/tom/test/NewTest.java
@@ -10,23 +10,7 @@ package com.tom.test;
 public class NewTest {
     public static void main(String[] args) {
         System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println("王斌");
-        System.out.println(11111);
-
-
-        System.out.println(989999);
-        System.out.println(555);
+       
 
     }
 }
