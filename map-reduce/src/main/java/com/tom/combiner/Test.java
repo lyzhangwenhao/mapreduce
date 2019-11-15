c915f1e563efc45175057fd388f3c89bc812d12a
wangbin <870921302@qq.com>
2019-11-15 20:45:12 +0800

    测试

---
+++ map-reduce/src/main/java/com/tom/combiner/Test.java
@@ -0,0 +1,13 @@
+package com.tom.combiner;
+
+/**
+ * @Author: 王彬
+ * @Date: 2019/11/15 20:44
+ * @Version 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("真帅");
+    }
+}
