483c5b14f716beb8419bede88b140ed09225b227
myMiDad <1277420858.c0m>
2019-11-15 21:05:16 +0800

    test

---
+++ map-reduce/src/main/java/com/tom/test/NewTest.java
@@ -0,0 +1,14 @@
+package com.tom.test;
+
+/**
+ * ClassName: NewTest
+ * Description:
+ *
+ * @author Mi_dad
+ * @date 2019/11/15 21:04
+ */
+public class NewTest {
+    public static void main(String[] args) {
+        System.out.println("王斌");
+    }
+}
